07d46905d1bbe3778b26a22eb25772ab6990904c
Adhali <omkar.a2989@gmail.com>
2022-03-23 08:37:22 -0400

    adding state and filter logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { robots } from './robots';
 import CardLists from './CardLists';
 import SearchBox from './SearchBox';
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardLists robots={robots}/>
-    </div>
-  )
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value)
+    this.setState({searchfield:event.target.value})
+  }
+
+  render(){
+    const filterrobot = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchfield={this.state.searchfield} searchChange={this.onSearchChange} />
+        <CardLists robots={filterrobot}/>
+      </div>
+    );
+  }
 }
 
 export default App;
